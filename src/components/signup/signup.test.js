eed4d948e018d321a2a58c911bfb2790cdc6aaf3
CodeXaver <mitchel.okomor@gmail.com>
2021-02-04 00:55:26 +0100

    removed test

--- src/components/signup/signup.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from "enzyme";
-import Signup from "./signup";
-
-describe("Signup component", () => {
-  it("should render a div for signup", () => {
-    const wrapper = render(<Signup />);
-    expect(wrapper.find("div")).toBeDefined();
-  });
-});
